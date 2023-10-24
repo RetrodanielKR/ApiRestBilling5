6969b95bbc12c75c2db444777f783c991397d389
Snnat19 <jnatsanchezrr@gmail.com>
2023-10-24 15:44:32 -0500

    feat(conf): add connection string and packages entity framework

---
+++ ApiRestBilling5/ApiRestBilling5/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestBilling5.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base (options)
+        {
+            
+        }
+    }
+}
