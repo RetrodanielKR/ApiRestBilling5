8c63e7cc1d6e015c0cbcc1b5c5bc99ecd6dca380
Snnat19 <jnatsanchezrr@gmail.com>
2023-10-25 16:12:34 -0500

    feat(conf):add validations to model class

--- ApiRestBilling5/ApiRestBilling5/Data/ApplicationDbContext.cs
+++ ApiRestBilling5/ApiRestBilling5/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApiRestBilling5.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiRestBilling5.Data
 {
@@ -9,5 +10,10 @@ namespace ApiRestBilling5.Data
         {
             
         }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
     }
 }
---
+++ ApiRestBilling5/ApiRestBilling5/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiRestBilling5.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(64)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(64)]
+        public string LastName { get; set; } = string.Empty;
+        [MaxLength(128)]
+        public string City { get; set; } = string.Empty;
+        [MaxLength(128)]
+        public string Country { get; set; } = string.Empty;
+        [MaxLength(16)]
+        public string Phone { get; set; } = string.Empty;
+        
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
