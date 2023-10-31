9d8016aeaa082d066d7f62d217e1d52737e98c5f
Snnat19 <jnatsanchezrr@gmail.com>
2023-10-31 15:43:43 -0500

    Contraladores Actualizados

--- ApiRestBilling5/ApiRestBilling5/Models/Order.cs
+++ ApiRestBilling5/ApiRestBilling5/Models/Order.cs
@@ -9,13 +9,12 @@ namespace ApiRestBilling5.Models
         public int Id { get; set; }
         [Required]
         public DateTime OrderDate { get; set; } = DateTime.Now;
-        [MaxLength(128)]
         public Guid OrderNumber { get; set; } = Guid.NewGuid();
         [Required]
         public int CustomerId { get; set; }
         public decimal TotalAmount { get; set; } = 0;
         [ForeignKey("CustomerId")]
-        public Customer Customer { get; set; }
+        public Customer? Customer { get; set; }
         public virtual ICollection<OrderItem> OrderItems { get; set; }
 
     }
--- ApiRestBilling5/ApiRestBilling5/Models/Product.cs
+++ ApiRestBilling5/ApiRestBilling5/Models/Product.cs
@@ -17,7 +17,7 @@ namespace ApiRestBilling5.Models
         public string Package { get; set; } = string.Empty;
         public bool IsDiscontinued { get; set; } = false;
         [ForeignKey("SupplierId")]
-        public Supplier Supplier { get; set; }
+        public Supplier? Supplier { get; set; }
         public ICollection<OrderItem>? OrderItems { get; set; }
 
     }
