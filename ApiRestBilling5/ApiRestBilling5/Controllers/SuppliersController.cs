3f94d8b7331376481b6439291fd31c8db2d7b98c
Snnat19 <jnatsanchezrr@gmail.com>
2023-11-01 17:34:49 -0500

    Finalizacion sin Order ptm

--- ApiRestBilling5/ApiRestBilling5/Controllers/SuppliersController.cs
+++ ApiRestBilling5/ApiRestBilling5/Controllers/SuppliersController.cs
@@ -3,10 +3,8 @@ using ApiRestBilling5.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ApiRestBilling5.Controllers
-{
+
     [Route("api/[controller]")]
     [ApiController]
     public class SuppliersController : ControllerBase
